80412372754ce04592cd9eedf10c0f2435ab67b0
Graciano Godoy <gracianogodoy@gmail.com>
2015-08-23 15:54:29 -0300

    Projeto arrumado

---
+++ Assets/Scripts/Gameplayer/Characters/Interfaces/IMovement.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMovement
+{
+    void Move(Vector2 direction, float speed);
+}
---
+++ Assets/Scripts/Gameplayer/Characters/Monster/Input.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Monster
+{
+    public class Input : IInput
+    {
+        private Transform _monsterTransform;
+
+        public Input(Transform transform)
+        {
+            _monsterTransform = transform;
+        }
+
+        public Vector2 GetAxis()
+        {
+            return direction();
+        }
+
+        private Vector2 direction()
+        {
+            Vector2 direction = mousePosition() - (Vector2)_monsterTransform.position;
+            return direction;
+        }
+
+        private Vector2 mousePosition()
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(UnityEngine.Input.mousePosition);
+            pos.z = 0;
+            Vector2 pos2d = pos;
+            return pos2d;
+        }
+
+    }
+}
