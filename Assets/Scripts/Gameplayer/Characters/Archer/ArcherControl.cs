26e355eadaaa03afb83c52ecd21b3877bafe81fb
Graciano Godoy <gracianogodoy@gmail.com>
2015-08-24 19:26:13 -0300

    Novos tiles adicionados

---
+++ Assets/Scripts/Gameplayer/Characters/Archer/ArcherControl.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Archer
+{
+    [Serializable]
+    public struct Settings
+    {
+        public float Speed;
+        public Transform Monster;
+        public float DistanceToFollow;
+    }
+
+    public enum State
+    {
+        Idle,
+        Moving
+    }
+
+    public class ArcherControl : MonoBehaviour
+    {
+        private IInput _input;
+        private IMovement _movement;
+        private State _state;
+
+        public Settings Settings;
+
+        void Awake()
+        {
+            _input = new IAInput(this);
+            _movement = new Peasant.Movement(GetComponent<Rigidbody2D>());
+            _state = State.Moving;
+        }
+
+        void FixedUpdate()
+        {
+            processState();
+        }
+
+        private void move()
+        {
+            Vector2 direction = _input.GetAxis();
+            _movement.Move(direction, Settings.Speed);
+        }
+
+        private void stop()
+        {
+            Vector2 direction = _input.GetAxis();
+
+            _movement.Move(direction, 0);
+        }
+
+        private void processState()
+        {
+            switch(_state)
+            {
+                case State.Idle:
+                    if (checkDistance() > Settings.DistanceToFollow)
+                    {
+                        _state = State.Moving;
+                    }
+
+                    stop();
+                    break;
+                case State.Moving:
+                    move();
+
+                    if (checkDistance() <= Settings.DistanceToFollow)
+                    {
+                        _state = State.Idle;
+                    }
+
+                    break;
+            }
+        }
+
+        private float checkDistance()
+        {
+            return Vector2.Distance(transform.position, Settings.Monster.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplayer/Characters/Archer/IAInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Archer
+{
+    public class IAInput : IInput
+    {
+        private ArcherControl _control;
+        private Vector2 _direction;
+
+        public IAInput(ArcherControl control)
+        {
+            _control = control;
+        }
+
+        public Vector2 GetAxis()
+        {
+            follow();
+
+            return _direction;
+        }
+
+        private float checkDistance()
+        {
+            return Vector2.Distance(_control.transform.position, _control.Settings.Monster.transform.position);
+        }
+
+        private void follow()
+        {
+            _direction = _control.Settings.Monster.transform.position - _control.transform.position;
+            _direction.Normalize();
+        }
+    }
+}
